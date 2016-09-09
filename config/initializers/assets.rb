f70c5bd0c0441d60fd278d8601cc4f061744c565
Leslie Albrecht <rooirampokker@gmail.com>
2016-09-09 10:20:35 +0200

    added jquery.simulate to deal with the automated drag-and-drop when an automatic game is triggered

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,6 +9,7 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( gridController.js )
+Rails.application.config.assets.precompile += %w( jquery.simulate.js )
 Rails.application.config.assets.precompile += %w( shipController.js )
 Rails.application.config.assets.precompile += %w( nukeController.js )
+Rails.application.config.assets.precompile += %w( gridController.js )
